dfabd7918fce642ffae21b3084664a36ccc4bf5f
CarlosXu-S <carlosxu0212@163.com>
2025-02-26 10:08:23 +0800

    fix:修改第一次点击“切换主题”的BUG，改为第一次点击“切换主题”，切换为Dark.

--- 1_list-hover-effect/main.js
+++ 1_list-hover-effect/main.js
@@ -4,15 +4,15 @@ const html = document.querySelector('html')
 var a = 0
 
 toggle.addEventListener('click',() => {
-    a++
+    ++a
     if (a % 2 === 0) {
-        html.classList.remove('light')
-        html.classList.add('dark')
-        toggle.innerHTML = 'Lighting'
-    } else {
         html.classList.remove('dark')
         html.classList.add('light')
         toggle.innerHTML = 'Dark'
+    } else {
+        html.classList.remove('light')
+        html.classList.add('dark')
+        toggle.innerHTML = 'Lighting'
     }
     console.log(a)
 
